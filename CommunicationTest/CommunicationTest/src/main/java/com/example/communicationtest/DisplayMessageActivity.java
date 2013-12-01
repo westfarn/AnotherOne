be0ff6348fd811a388f765e3cbb58473c000e0af
westfarn <ryan.westfall1@gmail.com>
2013-12-01 04:28:36 -0500

    initail Push

---
+++ CommunicationTest/CommunicationTest/src/main/java/com/example/communicationtest/DisplayMessageActivity.java
@@ -0,0 +1,172 @@
+package com.example.communicationtest;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.content.Intent;
+import android.widget.Button;
+
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+//gesture stuff
+
+
+public class DisplayMessageActivity extends Activity {
+    private HorizontalScrollView scrollViewer;
+    private LinearLayout linearLayout;
+    private Button questionBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.display_message);
+
+
+        if(Build.VERSION.SDK_INT >=Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            //show the up button in the action bar
+            getActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        addPictures();
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                //NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void previousPage(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+    private Boolean addPictures() {
+
+        //load images into the scroll viewer
+        scrollViewer = (HorizontalScrollView)findViewById(R.id.results);
+        int scroll_height = scrollViewer.getLayoutParams().height;
+        int scroll_width = scrollViewer.getLayoutParams().width;
+        linearLayout = (LinearLayout)findViewById(R.id.results_holder);
+        int layout_height = linearLayout.getLayoutParams().height;
+        int layout_width = linearLayout.getLayoutParams().width;
+        questionBtn = (Button)findViewById(R.id.question_box);
+        int btn_height = questionBtn.getHeight();
+        int btn_width = questionBtn.getWidth();
+
+        DisplayMetrics displayInfo = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayInfo);
+
+        int info_height = displayInfo.heightPixels;
+        int info_width = displayInfo.widthPixels;
+
+        int[] imgList;
+        imgList = new int[4];
+        imgList[0] = R.drawable.af_memorial;
+        imgList[1] = R.drawable.af_memorial1;
+        imgList[2] = R.drawable.af_memorial2;
+        imgList[3] = R.drawable.af_memorial3;
+        ImageView imgV = new ImageView(this);
+
+        //get the picture
+
+        String msg = "layout_width: " + Integer.toString(layout_width);
+        Log.i("App_Name",msg);
+        msg = "layout_height: " + Integer.toString(layout_height);
+        Log.i("App_Name",msg);
+        msg = "scroll_width: " + Integer.toString(scroll_width);
+        Log.i("App_Name",msg);
+        msg = "scroll_height: " + Integer.toString(scroll_height);
+        Log.i("App_Name",msg);
+        msg = "info_width: " + Integer.toString(info_width);
+        Log.i("App_Name",msg);
+        msg = "info_height: " + Integer.toString(info_height);
+        Log.i("App_Name",msg);
+        msg = "btn_width: " + Integer.toString(btn_width);
+        Log.i("App_Name",msg);
+        msg = "btn_height: " + Integer.toString(btn_height);
+        Log.i("App_Name",msg);
+
+        //set the picture to the imageView
+        //imgV.setImageBitmap(bmp);
+        for( int i = 0; i < imgList.length; i++) {
+            Bitmap bmp;
+            bmp = BitmapFactory.decodeResource(getResources(),imgList[i]);
+            bmp= Bitmap.createScaledBitmap(bmp, info_width - 100, info_height - 100, false);
+            imgV = new ImageView(this);
+            imgV.setImageBitmap(bmp);
+            imgV.setOnLongClickListener(this);
+            linearLayout.addView(imgV);
+        }
+        //scrollViewer.addView(imgV,0);
+        /*for(int i = 0; i < imgList.length; i++)
+        {
+            ImageView imgV = new ImageView(this);
+            //imgV.wid
+            imgV.setImageResource(imgList[i]);
+            linearLayout.addView(imgV);
+        }*/
+
+
+        return true;
+      
+    public boolean onLongClick(View v) {
+
+    }
+
+
+
+
+
+
+}
---
+++ CommunicationTest/CommunicationTest/src/main/java/com/example/communicationtest/MainActivity.java
@@ -0,0 +1,122 @@
+package com.example.communicationtest;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+//gesture stuff
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.GestureOverlayView.OnGesturePerformedListener;
+import android.gesture.Prediction;
+import android.widget.Toast;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.EditText;
+
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends Activity implements OnGesturePerformedListener {
+    private GestureLibrary gestureLib;
+    private Button btnSendSMS;
+    public final static String EXTRA_MESSAGE = "com.example.communicationtest.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSendSMS = (Button)findViewById(R.id.btnSendSMS);
+        btnSendSMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendSMS("5556","Hello my Friend");
+            }
+        });
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+    public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+        ArrayList<Prediction> predictions = gestureLib.recognize(gesture);
+        for (Prediction prediction : predictions) {
+            if (prediction.score > 1.0) {
+                Toast.makeText(this, prediction.name, Toast.LENGTH_SHORT).show();
+            }
+        }
+
+    }
+
+    private void sendSMS(String phoneNumber, String msg) {
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, msg, null, null);
+
+    }
+
+    public void nextPage(View v) { //for an onClick attribute for an object this must be public!
+        Intent intent;
+        intent = new Intent(this, com.example.communicationtest.DisplayMessageActivity.class);
+        //intent.setClass(this,com.example.communicationtest.DisplayMessageActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.editText);
+        String msg = "something";
+        intent.putExtra(EXTRA_MESSAGE, msg);
+        startActivity(intent);
+    }
+
+
+}
